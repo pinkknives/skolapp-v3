c673e84ce8828b852b317040e08ded51cffe6dd2
Max Marklund <49338634+pinkknives@users.noreply.github.com>
2025-09-21 18:21:17 +0000

    tailwind(v4): postcss v4 plugin, @import tailwindcss in globals, darkMode: 'class'

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,5 @@
-module.exports = {
-  plugins: {
-    '@tailwindcss/postcss': {},
-    autoprefixer: {},
-  },
+import tailwind from '@tailwindcss/postcss'
+
+export default {
+  plugins: [tailwind()],
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 const { heroui } = require("@heroui/theme");
 
 module.exports = {
+  darkMode: 'class',
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
